7946fc4347946f595cadd44461c636a189fb483d
JosueDSL <josuedesantosl@gmail.com>
2024-03-12 16:50:27 -0600

    new timer for the app

---
+++ timer.py
@@ -0,0 +1,25 @@
+# New timer
+
+import time
+import os
+import sys
+import datetime
+import signal
+
+def signal_handler(sig, frame):
+    print('You pressed Ctrl+C!')
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+def timer():
+    print("Please enter the time you want to set the timer for in minutes: ")
+    time_input = int(input())
+    time_input = time_input * 60
+    print("Timer set for " + str(time_input) + " seconds.")
+    time.sleep(time_input)
+    print("Time's up!")
+    os.system('aplay /home/pi/Downloads/Alarm.wav')
+    return
+
+timer()
