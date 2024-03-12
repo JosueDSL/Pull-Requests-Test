405426a36a9366d1589d40f01ca7f5f71e1c3f81
JosueDSL <josuedesantosl@gmail.com>
2024-03-12 14:22:43 -0600

    merge check after pulling from local

--- hello.py
+++ hello.py
@@ -1,2 +1,3 @@
 print("Hello Branch!")
 # Adding a change. Check if there are any merge conflits.
+## After pulling the changes from the master branch, I am adding this line to check if there are any merge conflicts.
